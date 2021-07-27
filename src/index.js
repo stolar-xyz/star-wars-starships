1f0eb2130e7d67a407167ba596fef8dffcd7abd0
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-27 15:49:57 +0200

    passed GlobalStyle and Normalize to render structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Normalize } from 'styled-normalize';
 import ReactDOM from 'react-dom';
-
 import App from './App/App';
+
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyle />
+    <Normalize />
     <App />
   </StrictMode>,
   document.getElementById('root')
