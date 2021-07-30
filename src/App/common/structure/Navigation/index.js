0e0288246ed1f3749d50e6813772ed2500dfc30a
Andrzej Stolarek <stolar.code@gmail.com>
2021-07-30 14:13:07 +0200

    added hooks and changeScrollCart function

--- src/App/common/structure/Navigation/index.js
+++ src/App/common/structure/Navigation/index.js
@@ -1,19 +1,32 @@
+import { useEffect, useState } from 'react';
 import { Heading, ItemsCounter, StyledNavigation, Wrapper } from './styled';
 import starWarsLogo from '../../../assets/svgs/starWarsLogo.svg';
 import shoppingCart from '../../../assets/svgs/shoppingCart.svg';
 
-const Navigation = ({ count }) => (
-  <StyledNavigation>
-    <header>
-      <Heading>
-        <img alt='Star Wars' src={starWarsLogo} width='160' height='80' />
-      </Heading>
-    </header>
-    <Wrapper>
-      <img alt='Shopping cart' src={shoppingCart} width='52' height='52' />
-      {count && <ItemsCounter>{count > 10 ? '+10' : count}</ItemsCounter>}
-    </Wrapper>
-  </StyledNavigation>
-);
+const Navigation = ({ count }) => {
+  const [scrollCart, setScrollCart] = useState(false);
+
+  const changeScrollCart = () => {
+    window.scrollY >= 80 ? setScrollCart(true) : setScrollCart(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeScrollCart);
+  }, []);
+
+  return (
+    <StyledNavigation>
+      <header>
+        <Heading>
+          <img alt='Star Wars' src={starWarsLogo} width='160' height='80' />
+        </Heading>
+      </header>
+      <Wrapper>
+        <img alt='Shopping cart' src={shoppingCart} width='52' height='52' />
+        {count && <ItemsCounter>{count > 10 ? '+10' : count}</ItemsCounter>}
+      </Wrapper>
+    </StyledNavigation>
+  );
+};
 
 export default Navigation;
