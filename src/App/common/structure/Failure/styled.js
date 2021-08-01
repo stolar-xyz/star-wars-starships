ae1c342a87ea0c5f7fde06087d87ef5554fb0be2
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 13:51:53 +0200

    added styled components for Failure

---
+++ src/App/common/structure/Failure/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { ReactComponent as DarthVader } from '../../../assets/svgs/darthVader.svg';
+
+export const Container = styled.div`
+  margin-top: 10%;
+  text-align: center;
+`;
+
+export const DarthVaderIcon = styled(DarthVader)`
+  margin-bottom: 24px;
+`;
+
+export const Description = styled.p`
+  color: #ffffff;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.4;
+`;
