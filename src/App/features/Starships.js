bb4c7bdeb1a11e0f4291af0af30f27f8c9214a89
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 14:08:58 +0200

    added useQuery hook and changed structure

--- src/App/features/Starships.js
+++ src/App/features/Starships.js
@@ -1,16 +1,44 @@
+import { gql, useQuery } from '@apollo/client';
+import { query } from '../../apiDetails';
+import Failure from '../common/structure/Failure';
+import Loading from '../common/structure/Loading';
 import Tile from '../common/structure/Tile';
 import List from '../common/styled/List';
-import Section from '../common/styled/Section';
 import Subheader from '../common/styled/Subheader';
 
 const Starships = () => {
+  const spaceshipsQuery = gql(query);
+
+  const { data, loading, error } = useQuery(spaceshipsQuery, {
+    variables: {
+      items: 36,
+    },
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Failure />;
+  }
+
   return (
-    <Section>
+    <>
       <Subheader>Starships</Subheader>
       <List>
-        <Tile />
+        {data.allStarships.starships.map(
+          ({ name, manufacturers, costInCredits }) => (
+            <Tile
+              key={name}
+              name={name}
+              manufacturers={manufacturers}
+              cost={costInCredits}
+            />
+          )
+        )}
       </List>
-    </Section>
+    </>
   );
 };
 
