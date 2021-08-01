c900023ade7338e9a4501c6fcd82aa33825382e1
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 18:08:02 +0200

    deleted prop and added useReactiveVar with totalItemsVar

--- src/App/common/structure/Navigation/index.js
+++ src/App/common/structure/Navigation/index.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import { useReactiveVar } from '@apollo/client';
+import { totalItemsVar } from '../../../reactiveVariables';
 import { Heading, ItemsCounter, StyledNavigation, Wrapper } from './styled';
 import starWarsLogo from '../../../assets/svgs/starWarsLogo.svg';
 import shoppingCart from '../../../assets/svgs/shoppingCart.svg';
 
-const Navigation = ({ count }) => {
+const Navigation = () => {
+  const count = useReactiveVar(totalItemsVar);
   const [scrollCart, setScrollCart] = useState(false);
 
   const changeScrollCart = () => {
