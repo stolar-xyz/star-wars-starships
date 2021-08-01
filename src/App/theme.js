1acbb5070ed076598aaa55b8b85965c54a1332c8
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 22:52:03 +0200

    added colors and breakpoints to theme

--- src/App/theme.js
+++ src/App/theme.js
@@ -1,1 +1,35 @@
-export const theme = {};
+const breakpoints = {
+  mobileMax: '767',
+  tabletMax: '1023',
+};
+
+const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  silver: '#cdcdcd',
+};
+
+export const theme = {
+  color: {
+    text: {
+      primaryText: colors.white,
+      secondaryText: colors.black,
+    },
+
+    background: {
+      primaryBackground: colors.white,
+      secondaryBackground: colors.black,
+      tertiaryBackground: '#fcd420',
+      disabledBackground: colors.silver,
+    },
+
+    element: {
+      listBullet: '#ff9f00',
+      buttonBoxShadow: '#e4e6f0',
+      cartBoxShadow: colors.silver,
+      inputBorder: '#808080',
+    },
+  },
+
+  ...breakpoints,
+};
