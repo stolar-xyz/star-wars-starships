13b120f0d8e774bb9bf5ffdfd1ce5220e8eb9e80
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 13:51:37 +0200

    added Failure component

---
+++ src/App/common/structure/Failure/index.js
@@ -0,0 +1,13 @@
+import Subheader from '../../styled/Subheader';
+
+const Failure = () => (
+  <div>
+    <Subheader>Ooops! Something went wrong...</Subheader>
+    <p>
+      Please check your network connection
+      <br /> and try again
+    </p>
+  </div>
+);
+
+export default Failure;
