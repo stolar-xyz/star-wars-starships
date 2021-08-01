f080f9f76cd82ba350178c3f7a4e385a46ae39c3
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-01 22:07:11 +0200

    wrapped App structure with ThemeProvider

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 import Navigation from './common/structure/Navigation';
 import Section from './common/styled/Section';
 import Starships from './features/Starships';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Navigation />
       <main>
         <Section>
           <Starships />
         </Section>
       </main>
-    </>
+    </ThemeProvider>
   );
 };
 
