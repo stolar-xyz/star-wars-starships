32c948812c622eb5bf9c576e3c8bf733010601f7
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-02 17:13:54 +0200

    changed structure and added Cart component

--- src/App/common/structure/Navigation/index.js
+++ src/App/common/structure/Navigation/index.js
@@ -1,46 +1,16 @@
-import { useEffect, useState } from 'react';
-import { useReactiveVar } from '@apollo/client';
-import { totalItemsVar } from '../../../reactiveVariables';
-import {
-  Heading,
-  ItemsCounter,
-  Logo,
-  ShoppingCart,
-  StyledNavigation,
-  Wrapper,
-} from './styled';
+import { Heading, Logo, StyledNavigation } from './styled';
+import Cart from '../Cart';
 import starWarsLogo from '../../../assets/svgs/starWarsLogo.svg';
-import shoppingCart from '../../../assets/svgs/shoppingCart.svg';
 
-const Navigation = () => {
-  const count = useReactiveVar(totalItemsVar);
-  const [scrollCart, setScrollCart] = useState(false);
-
-  const changeScrollCart = () => {
-    window.scrollY >= 80 ? setScrollCart(true) : setScrollCart(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', changeScrollCart);
-  }, []);
-
-  return (
-    <StyledNavigation>
-      <header>
-        <Heading>
-          <Logo alt='Star Wars' src={starWarsLogo} />
-        </Heading>
-      </header>
-      <Wrapper scrollCart={scrollCart}>
-        <ShoppingCart alt='Shopping cart' src={shoppingCart} />
-        {count >= 1 && (
-          <ItemsCounter scrollCart={scrollCart}>
-            {count > 99 ? '+99' : count}
-          </ItemsCounter>
-        )}
-      </Wrapper>
-    </StyledNavigation>
-  );
-};
+const Navigation = () => (
+  <StyledNavigation>
+    <header>
+      <Heading>
+        <Logo alt='Star Wars' src={starWarsLogo} />
+      </Heading>
+    </header>
+    <Cart />
+  </StyledNavigation>
+);
 
 export default Navigation;
