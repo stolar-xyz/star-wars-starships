5677ab81739cc1f50206bef82a91066285045665
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-02 17:12:36 +0200

    deleted unused styled components

--- src/App/common/structure/Navigation/styled.js
+++ src/App/common/structure/Navigation/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const StyledNavigation = styled.nav`
   display: flex;
@@ -23,74 +23,5 @@ export const Logo = styled.img`
   @media (max-width: ${({ theme }) => theme.mobileMax}px) {
     width: 140px;
     height: 60px;
-  }
-`;
-
-export const ShoppingCart = styled.img`
-  width: 52px;
-  height: 52px;
-
-  @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    width: 40px;
-    height: 40px;
-  }
-`;
-
-export const Wrapper = styled.div`
-  position: ${({ scrollCart }) => (scrollCart ? 'fixed' : 'absolute')};
-  right: ${({ scrollCart }) => (scrollCart ? '40px' : '25px')};
-  transition: ${({ scrollCart }) =>
-    scrollCart
-      ? ' background-color 0.5s, right 0.5s, box-shadow 0.5s'
-      : 'right 0.5s'};
-
-  @media (max-width: ${({ theme }) => theme.tabletMax}px) {
-    right: 15px;
-  }
-
-  ${({ scrollCart }) =>
-    scrollCart &&
-    css`
-      box-shadow: 0px 0px 5px ${({ theme }) => theme.color.element.cartBoxShadow};
-      bottom: 40px;
-      width: 80px;
-      height: 80px;
-      display: grid;
-      place-items: center;
-      background: ${({ theme }) => theme.color.background.primaryBackground};
-      border-radius: 10px;
-      z-index: 1;
-
-      @media (max-width: ${({ theme }) => theme.tabletMax}px) {
-        bottom: 15px;
-      }
-
-      @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-        width: 65px;
-        height: 65px;
-      }
-    `}
-`;
-
-export const ItemsCounter = styled.span`
-  display: grid;
-  place-items: center;
-  background: ${({ theme }) => theme.color.background.tertiaryBackground};
-  color: ${({ theme }) => theme.color.text.secondaryText};
-  border-radius: 50%;
-  width: 23px;
-  height: 23px;
-  position: absolute;
-  top: ${({ scrollCart }) => (scrollCart ? '45px' : '30px')};
-  right: ${({ scrollCart }) => (scrollCart ? '40px' : '25px')};
-  font-size: 12px;
-  font-weight: 700;
-
-  @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    font-size: 10px;
-    width: 20px;
-    height: 20px;
-    top: ${({ scrollCart }) => (scrollCart ? '35px' : '20px')};
-    right: ${({ scrollCart }) => (scrollCart ? '30px' : '20px')};
   }
 `;
--- src/App/common/structure/Tile/styled.js
+++ src/App/common/structure/Tile/styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Button from '../../styled/Button';
 
 export const StyledTile = styled.li`
   background: ${({ theme }) => theme.color.background.primaryBackground};
@@ -66,41 +65,4 @@ export const Cost = styled.span`
 export const Wrapper = styled.div`
   margin: 32px 0;
   text-align: center;
-`;
-
-export const CountButtons = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 16px;
-`;
-
-export const CountButton = styled(Button)`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  font-weight: 700;
-
-  @media (max-width: ${({ theme }) => theme.mobileMax}px) {
-    width: 35px;
-    height: 35px;
-  }
-`;
-
-export const Count = styled.input`
-  width: 80px;
-  border: 1px solid ${({ theme }) => theme.color.element.inputBorder};
-  border-radius: 5px;
-  text-align: center;
-  -moz-appearance: textfield;
-
-  &::-webkit-outer-spin-button,
-  &::-webkit-inner-spin-button {
-    appearance: none;
-  }
-`;
-
-export const AddCartButton = styled(Button)`
-  padding: 8px 16px;
-  border-radius: 5px;
-  width: 100%;
 `;
