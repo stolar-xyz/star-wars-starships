c7420fe4d855c0e2cd9b57277028dc0650f7bebc
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-02 17:14:18 +0200

    changed structure and added AddToCart component with prop

--- src/App/common/structure/Tile/index.js
+++ src/App/common/structure/Tile/index.js
@@ -1,92 +1,42 @@
-import { useState } from 'react';
-import { totalItemsVar } from '../../../reactiveVariables';
 import {
-  AddCartButton,
   Name,
   Cost,
   ManufacturersList,
   StyledTile,
-  Count,
-  CountButton,
-  CountButtons,
   Wrapper,
   Information,
 } from './styled';
+import AddToCart from '../AddToCart';
 
-const Tile = ({ name, manufacturers, cost }) => {
-  const [quantity, setQuantity] = useState(1);
-
-  const onFormSubmit = event => {
-    event.preventDefault();
-    totalItemsVar(totalItemsVar() + +quantity);
-    setQuantity(1);
-  };
-
-  return (
-    <StyledTile>
-      <div>
-        <Name>{name}</Name>
-        {manufacturers && (
+const Tile = ({ name, manufacturers, cost }) => (
+  <StyledTile>
+    <div>
+      <Name>{name}</Name>
+      {manufacturers && (
+        <>
+          <Information>Manufacturers</Information>
+          <ManufacturersList>
+            {manufacturers.map(manufacturer => (
+              <li key={manufacturer}>{manufacturer}</li>
+            ))}
+          </ManufacturersList>
+        </>
+      )}
+    </div>
+    <div>
+      <Wrapper>
+        {cost ? (
           <>
-            <Information>Manufacturers</Information>
-            <ManufacturersList>
-              {manufacturers.map(manufacturer => (
-                <li key={manufacturer}>{manufacturer}</li>
-              ))}
-            </ManufacturersList>
+            <Information>Cost</Information>
+            <Cost>{cost}</Cost>
           </>
+        ) : (
+          <Information>Unavailable</Information>
         )}
-      </div>
-      <div>
-        <Wrapper>
-          {cost ? (
-            <>
-              <Information>Cost</Information>
-              <Cost>{cost}</Cost>
-            </>
-          ) : (
-            <Information>Unavailable</Information>
-          )}
-        </Wrapper>
-        <form onSubmit={onFormSubmit}>
-          <CountButtons>
-            <CountButton
-              type='button'
-              title='Decrease'
-              disabled={quantity <= 1}
-              onClick={() => {
-                setQuantity(+quantity - 1);
-              }}
-            >
-              -
-            </CountButton>
-            <Count
-              type='number'
-              min='1'
-              required
-              placeholder='Quantity'
-              value={quantity}
-              onChange={({ target }) => {
-                setQuantity(target.value);
-              }}
-            />
-            <CountButton
-              type='button'
-              title='Increase'
-              onClick={() => {
-                setQuantity(+quantity + 1);
-              }}
-            >
-              +
-            </CountButton>
-          </CountButtons>
-          <AddCartButton title='Add to shopping cart' disabled={!cost}>
-            Add to cart
-          </AddCartButton>
-        </form>
-      </div>
-    </StyledTile>
-  );
-};
+      </Wrapper>
+      <AddToCart cost={cost} />
+    </div>
+  </StyledTile>
+);
 
 export default Tile;
