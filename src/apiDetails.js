7357ce6604c04f000741ceece3beb3fe6d8cb5c8
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-02 15:15:35 +0200

    deleted totalCount from query

--- src/apiDetails.js
+++ src/apiDetails.js
@@ -2,7 +2,6 @@ export const uri = 'https://swapi.apis.guru/';
 export const query = `
   query GetSpaceships($items: Int!) {
     allStarships(first: $items) {
-      totalCount
       starships {
         name
         manufacturers
