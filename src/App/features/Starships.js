0945fca637890674d2e131a459a7baec4d84f694
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-09 20:30:36 +0200

    added useState hook with itemsCount and added Button which sets totalCount

--- src/App/features/Starships.js
+++ src/App/features/Starships.js
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { query } from '../../apiDetails';
 import Failure from '../common/structure/Failure';
 import Loading from '../common/structure/Loading';
 import Tile from '../common/structure/Tile';
+import Button from '../common/styled/Button';
 import List from '../common/styled/List';
 import Subheader from '../common/styled/Subheader';
 
 const Starships = () => {
+  const [itemsCount, setItemsCount] = useState(18);
+
   const starshipsQuery = gql(query);
 
   const { data, loading, error } = useQuery(starshipsQuery, {
     variables: {
-      items: 36,
+      items: itemsCount,
     },
   });
 
@@ -22,6 +26,8 @@ const Starships = () => {
   if (error) {
     return <Failure />;
   }
+
+  const totalItemsCount = data.allStarships.totalCount;
 
   return (
     <>
@@ -38,6 +44,17 @@ const Starships = () => {
           )
         )}
       </List>
+      {itemsCount !== totalItemsCount && (
+        <Button
+          loadButton
+          onClick={() => {
+            setItemsCount(totalItemsCount);
+          }}
+          title={'Load all'}
+        >
+          Load all
+        </Button>
+      )}
     </>
   );
 };
