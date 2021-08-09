10034619da5c70984c4283816e2d29a45b465141
Andrzej Stolarek <stolar.code@gmail.com>
2021-08-09 19:40:53 +0200

    added styles on loadButton prop

--- src/App/common/styled/Button/index.js
+++ src/App/common/styled/Button/index.js
@@ -1,11 +1,22 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   border: none;
   background: ${({ theme }) => theme.color.background.tertiaryBackground};
-  box-shadow: 0px 2px 12px ${({ theme }) => theme.color.element.buttonBoxShadow};
+  box-shadow: ${({ loadButton, theme }) =>
+    !loadButton && `0px 2px 12px ${theme.color.element.buttonBoxShadow}`};
   cursor: pointer;
   transition: transform 0.3s, background-color 0.3s;
+
+  ${({ loadButton }) =>
+    loadButton &&
+    css`
+      display: block;
+      margin: 0 auto;
+      min-width: 25%;
+      padding: 8px 22px;
+      border-radius: 5px;
+    `}
 
   &:disabled {
     cursor: unset;
